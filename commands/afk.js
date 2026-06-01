3e1ca3e6e2bcf599c91f229d55b1270e393f655b
senvatorplayzrblox-sudo <senvatorplayzrblox@gmail.com>
2026-06-01 20:47:28 +0530

    Update afk.js

--- commands/afk.js
+++ commands/afk.js
@@ -1,1 +1,26 @@
-module.exports = {};
+const fs = require("fs");
+
+module.exports = {
+  name: "afk",
+
+  execute(message, users, args) {
+    const reason = args.join(" ") || "AFK";
+
+    const afk = JSON.parse(
+      fs.readFileSync("./data/afk.json", "utf8")
+    );
+
+    afk[message.author.id] = {
+      reason,
+      since: Date.now(),
+      mentions: []
+    };
+
+    fs.writeFileSync(
+      "./data/afk.json",
+      JSON.stringify(afk, null, 2)
+    );
+
+    message.reply(`💤 You are now AFK: ${reason}`);
+  }
+};
