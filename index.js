207e04135b2d985a31a8f99b7308da44a8702dcb
senvatorplayzrblox-sudo <senvatorplayzrblox@gmail.com>
2026-06-01 17:23:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+const { Client, GatewayIntentBits } = require('discord.js');
 
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers
+  ]
+});
+
+client.once('ready', () => {
+  console.log(`🌿 ${client.user.tag} is online!`);
+});
+
+client.on('messageCreate', async (message) => {
+  if (message.author.bot) return;
+
+  if (message.content === '$ping') {
+    message.reply('🏓 Pong!');
+  }
+});
+
+client.login('PASTE_YOUR_BOT_TOKEN_HERE');
