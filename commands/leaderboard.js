eef10b2707d20bf80176f3dbdfc6f84591cd9a16
senvatorplayzrblox-sudo <senvatorplayzrblox@gmail.com>
2026-06-01 20:26:03 +0530

    Update leaderboard.js

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -1,3 +1,17 @@
 module.exports = {
-  name: "leaderboard"
+  name: "leaderboard",
+  execute(message, users) {
+
+    const sorted = Object.entries(users)
+      .sort((a, b) => b[1].points - a[1].points)
+      .slice(0, 10);
+
+    let text = "🏆 Clover Leaderboard\n\n";
+
+    sorted.forEach((user, index) => {
+      text += `${index + 1}. <@${user[0]}> - ${user[1].points} points\n`;
+    });
+
+    message.reply(text);
+  }
 };
