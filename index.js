04c0fb3a7916960717bc633d7fd07a30d73b12fb
senvatorplayzrblox-sudo <senvatorplayzrblox@gmail.com>
2026-06-01 19:48:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,41 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, Collection } = require("discord.js");
+const fs = require("fs");
 
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildMembers
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildPresences
   ]
 });
 
-client.once('ready', () => {
+client.commands = new Collection();
+
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter(file => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(file.replace(".js", ""), command);
+}
+
+const eventFiles = fs
+  .readdirSync("./events")
+  .filter(file => file.endsWith(".js"));
+
+for (const file of eventFiles) {
+  const event = require(`./events/${file}`);
+  if (event.execute) {
+    event.execute(client);
+  }
+}
+
+client.once("ready", () => {
   console.log(`🌿 ${client.user.tag} is online!`);
 });
 
-client.on('messageCreate', async (message) => {
-  if (message.author.bot) return;
-
-  if (message.content === '$ping') {
-    message.reply('🏓 Pong!');
-  }
-});
-
-client.login('PASTE_YOUR_BOT_TOKEN_HERE');
+client.login(process.env.TOKEN);
