f55a4cb4181faae1a33bb70a5bb43dfe2f43dcba
senvatorplayzrblox-sudo <senvatorplayzrblox@gmail.com>
2026-06-02 18:27:06 +0530

    Update store.js

--- commands/store.js
+++ commands/store.js
@@ -1,8 +1,26 @@
+const fs = require("fs");
+
 module.exports = {
   name: "store",
+
   execute(message) {
-    message.reply(
-      "🌿 Clover Store\n\nNo roles have been added yet."
+
+    const shop = JSON.parse(
+      fs.readFileSync("./data/shop.json", "utf8")
     );
+
+    if (!shop.roles.length) {
+      return message.reply(
+        "🛒 Store is empty."
+      );
+    }
+
+    let text = "🛒 Clover Store\n\n";
+
+    shop.roles.forEach((role, index) => {
+      text += `${index + 1}. ${role.name} - ${role.price} points\n`;
+    });
+
+    message.reply(text);
   }
 };
