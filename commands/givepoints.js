8b9461acc2427aa0a950034026dcbf113d26f07a
senvatorplayzrblox-sudo <senvatorplayzrblox@gmail.com>
2026-06-08 10:45:06 +0530

    Create givepoints.js

---
+++ commands/givepoints.js
@@ -0,0 +1,58 @@
+const fs = require("fs");
+const { EmbedBuilder } = require("discord.js");
+
+module.exports = {
+  name: "givepoints",
+
+  execute(message, users, args) {
+
+    const OWNER_ID = "1418875905085935637";
+
+    if (message.author.id !== OWNER_ID) {
+      return message.reply("❌ Owner only.");
+    }
+
+    const member =
+      message.mentions.members.first();
+
+    const amount = parseInt(args[1]);
+
+    if (!member || isNaN(amount)) {
+      return message.reply(
+        "Usage: `$givepoints @user <amount>`"
+      );
+    }
+
+    if (!users[member.id]) {
+      users[member.id] = {
+        points: 0,
+        messages: 0,
+        vcMinutes: 0
+      };
+    }
+
+    users[member.id].points += amount;
+
+    const allUsers = JSON.parse(
+      fs.readFileSync("./data/users.json", "utf8")
+    );
+
+    allUsers[message.guild.id] = users;
+
+    fs.writeFileSync(
+      "./data/users.json",
+      JSON.stringify(allUsers, null, 2)
+    );
+
+    message.reply({
+      embeds: [
+        new EmbedBuilder()
+          .setTitle("✅ Points Added")
+          .setDescription(
+            `👤 ${member.user.username}\n⭐ Added: ${amount}`
+          )
+      ]
+    });
+
+  }
+};
