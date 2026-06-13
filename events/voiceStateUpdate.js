d8a0e4d5f174ad6246f062b6b423a634b0168886
senvatorplayzrblox-sudo <senvatorplayzrblox@gmail.com>
2026-06-13 18:36:54 +0530

    Update voiceStateUpdate.js

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 
 const joined = new Map();
+const activeVC = new Map();
 
 module.exports.execute = (client) => {
 
@@ -12,6 +13,49 @@ module.exports.execute = (client) => {
     if (!oldState.channelId && newState.channelId) {
 
       joined.set(userId, Date.now());
+      const users = JSON.parse(
+  fs.readFileSync("./data/users.json", "utf8")
+);
+
+if (!users[guildId]) users[guildId] = {};
+
+if (!users[guildId][userId]) {
+  users[guildId][userId] = {
+    points: 0,
+    messages: 0,
+    vcMinutes: 0
+  };
+}
+
+fs.writeFileSync(
+  "./data/users.json",
+  JSON.stringify(users, null, 2)
+);
+      const interval = setInterval(() => {
+
+  const users = JSON.parse(
+    fs.readFileSync("./data/users.json", "utf8")
+  );
+
+  if (!users[guildId]) return;
+  if (!users[guildId][userId]) return;
+
+  users[guildId][userId].vcMinutes++;
+
+  if (
+    users[guildId][userId].vcMinutes % 5 === 0
+  ) {
+    users[guildId][userId].points++;
+  }
+
+  fs.writeFileSync(
+    "./data/users.json",
+    JSON.stringify(users, null, 2)
+  );
+
+}, 60000);
+
+activeVC.set(userId, interval);
 
     }
 
@@ -21,11 +65,9 @@ module.exports.execute = (client) => {
 
       if (!joinTime) return;
 
-      const minutes = Math.floor(
-        (Date.now() - joinTime) / 60000
-      );
-
       joined.delete(userId);
+      clearInterval(activeVC.get(userId));
+activeVC.delete(userId);
 
       const users = JSON.parse(
         fs.readFileSync("./data/users.json", "utf8")
@@ -35,12 +77,7 @@ module.exports.execute = (client) => {
 
 if (!users[guildId][userId]) return;
 
-      users[guildId][userId].vcMinutes += minutes;
-
-      users[guildId][userId].points += Math.floor(
-  minutes / 5
-);
-
+    
       fs.writeFileSync(
         "./data/users.json",
         JSON.stringify(users, null, 2)
